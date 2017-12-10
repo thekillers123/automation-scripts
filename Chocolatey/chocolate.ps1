44115257aa2c34f95b0d19758fb37c91f418c238
rookiepeng <rookie.dev@gmail.com>
2017-12-10 13:18:47 -0600

    my chocolatey script

---
+++ Chocolatey/chocolate.ps1
@@ -0,0 +1,21 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install adobereader-update
+choco install ccleaner
+choco install 7zip
+choco install ghostscript
+choco install visualstudiocode
+
+choco install mendeley
+
+choco install gitkraken
+choco install github-desktop
+
+choco install androidstudio
+#choco install google-backup-and-sync
+choco install hyper
+
+choco install anaconda3
+choco install pycharm-community
+
+choco install nodejs
