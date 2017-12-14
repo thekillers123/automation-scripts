437d7c4f711ce127fd1ccce5862b1d061f95dd61
rookiepeng <rookie.dev@gmail.com>
2017-12-14 16:45:31 -0600

    update script

--- Chocolatey/chocolate.ps1
+++ Chocolatey/chocolate.ps1
@@ -5,26 +5,69 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 # choco install wox
 
+"
+Install Tools:
+- Foxit Reader
+- CCleaner
+- 7zip
+- GhostScript
+- VSCode
+- Mendeley
+- Hyper
+- Screentogif
+"
+$ANS = Read-Host -Prompt '[Y]es or [N]o?'
+if ($ANS -eq "Y" -or $ANS -eq "y")
+{
 # choco install adobereader-update
 choco install foxitreader
-
 choco install ccleaner
 choco install 7zip
 choco install ghostscript
 choco install visualstudiocode
-
 choco install mendeley
+choco install hyper
+choco install screentogif
+}
 
+"
+Install git:
+- GitKraken
+- GitHub
+"
+$ANS = Read-Host -Prompt '[Y]es or [N]o?'
+if ($ANS -eq "Y" -or $ANS -eq "y")
+{
 choco install gitkraken
 choco install github-desktop
+}
 
+"
+Install Android Studio
+"
+$ANS = Read-Host -Prompt '[Y]es or [N]o?'
+if ($ANS -eq "Y" -or $ANS -eq "y")
+{
 choco install androidstudio
-choco install hyper
+}
 
+"
+Install Python
+"
+$ANS = Read-Host -Prompt '[Y]es or [N]o?'
+if ($ANS -eq "Y" -or $ANS -eq "y")
+{
 choco install anaconda3
 choco install pycharm-community
+}
 
+"
+Install Node.js
+"
+$ANS = Read-Host -Prompt '[Y]es or [N]o?'
+if ($ANS -eq "Y" -or $ANS -eq "y")
+{
 choco install nodejs
+}
 
-choco install screentogif
 
