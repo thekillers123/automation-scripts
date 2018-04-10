a34759bdc267b7663fd944cd708d505ffdb53df9
rookiepeng <rookie.dev@gmail.com>
2018-04-10 12:47:57 -0500

    rename bat script and add comments

--- Chocolatey/run.bat
+++
@@ -1,1 +0,0 @@
-powershell -ExecutionPolicy Bypass -File "chocolate.ps1"
---
+++ Chocolatey/runScript.bat
@@ -0,0 +1,9 @@
+::
+:: This script is used to run the powershell script <chocolate.ps1>
+:: **Administration is required**
+::
+:: By: Zach Peng
+:: https://zpeng.me
+::
+
+powershell -ExecutionPolicy Bypass -File "chocolate.ps1"
