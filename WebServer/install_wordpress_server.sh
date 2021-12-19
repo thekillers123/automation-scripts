3992996f2ffd9389d64812ddd5f2c0827c1b2803
thekillers123 <azriff456@gmail.com>
2021-12-19 16:41:33 +0800

    Update install_wordpress_server.sh

--- WebServer/install_wordpress_server.sh
+++ WebServer/install_wordpress_server.sh
@@ -10,8 +10,7 @@ apt update && apt upgrade
 apt install mariadb-server mariadb-client
 #apt install apache2 apache2-mod-php7.3
 apt install nginx
-apt install php7.3 php7.3-mysql php7.3-curl php7.3-xml php7.3-zip \
-php7.3-gd php7.3-mbstring php7.3-pspell php7.3-cgi php7.3-xmlrpc php7.3-imap php7.3-bcmath php7.3-imagick php7.3-fpm
+apt install php7.3 php7.3-mysql php7.3-curl php7.3-xml php7.3-zip php7.3-gd php7.3-mbstring php7.3-pspell php7.3-cgi php7.3-xmlrpc php7.3-imap php7.3-bcmath php7.3-imagick php7.3-fpm
 
 
 apt install php-apcu
